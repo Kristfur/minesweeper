1a443cd98daad1514d2616b2886a88784d228152
Kristfur <chrislonergann@gmail.com>
2023-03-29 13:22:33 +0000

    Add game board and generate an empty board

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons){
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "start"){
-                generateBoard();
+                displayBoard();
             } else if (this.getAttribute("data-type") === "rules"){
                 displayRules();
             } else if (this.getAttribute("data-type") === "leaderboard"){
@@ -30,12 +30,14 @@ function displayHome(){
     document.getElementById('home-page').style.display = 'block';
 }
 
-function generateBoard(){
+function displayBoard(){
     let pages = document.getElementsByClassName("page");
 
     for (let page of pages){
         page.style.display = 'none';
     }
+
+    generateBoard();
 
     document.getElementById('game-page').style.display = 'block';
 }
@@ -52,4 +54,40 @@ function displayLeaderboard(){
     }
 
     document.getElementById('leaderboard-page').style.display = 'block';
+}
+
+/**
+ * Generates a new random board based on the game settings
+ */
+function generateBoard(){
+    let gridSize = 5;   //******
+    let board = []
+
+    //Generate empty 2d array: 'board'
+    for (let o = 0; o < gridSize; o++){
+        let boardRow = []
+        for (let i = 0; i < gridSize; i++){
+            boardRow[i] = 0;
+        }
+        board[o] = boardRow;
+    }
+
+    //Randomly populate board with a set number mines
+    //   *Mines are represented by the number 9
+    let totalMines = 4;   //******
+    let mineCount = 0;
+    while(mineCount < totalMines){
+        let mineX = Math.floor(Math.random() * gridSize);
+        let mineY = Math.floor(Math.random() * gridSize); 
+
+        if(board[mineY][mineX] == 0){
+            board[mineY][mineX] = 9;
+            mineCount++;
+        }
+    }
+
+    console.log(board);
+    for (let i = 0; i < gridSize; i++){
+        document.getElementById("game-board").innerHTML += `<p>${board[i]}</p>`;
+    }
 }
